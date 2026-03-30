7b5d2c1c96dc01d0e168763241dee071a7f25252
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2026-03-30 01:32:04 +0000

    cleans up font load and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,27 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'a11y-examples',
-			// GitHub Pages URL
-			site: 'https://shkeating.github.io', 
-			// repository name
-			base: '/a11y-examples',
-			customCss: [
-				'./src/styles/global.css',
-			],
-			social: [
-				{
-				label: 'repo link',
-				icon: 'github',
-				href: 'https://github.com/shkeating/a11y-examples',
-    			},
-			],
-			sidebar: [
-				{
-					label: 'HTML Tables',
-					autogenerate: { directory: 'tables' },
-				},
-			],
-		}),
-	],
+  site: "https://shkeating.github.io",
+  base: "/a11y-examples",
+  integrations: [
+    starlight({
+      title: "a11y-examples",
+      customCss: ["./src/styles/global.css"],
+      social: [
+        {
+          label: "repo link",
+          icon: "github",
+          href: "https://github.com/shkeating/a11y-examples",
+        },
+      ],
+      sidebar: [
+        {
+          label: "HTML Tables",
+          autogenerate: { directory: "tables" },
+        },
+      ],
+    }),
+  ],
 });
